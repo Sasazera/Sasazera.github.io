fef494ad5b10538ba44d9abfa7dc9f4c10d3bed2
Sayumi Sakamoto <sayumisakamoto@MacBook-Pro-de-admin.local>
2020-09-03 16:40:21 +0900

    Criando a cobrinha

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
-//Background
+//BACKGROUND
 function BG() {
     context.fillStyle = "lightblue";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
+//cria a cobrinha 
+function snakeborn(){
+    for(i=0; i <snake.length; i++){
+        context.fillStyle = "Darkblue"
+        context.fillRect(snake[i].x, snake[i].y, box, box); 
+    }
+
+}
 
 BG();
+snakeborn();
